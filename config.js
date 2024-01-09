1bbcbce0941164079a37bf4759cf971ff7b2c41c
Yeachan Kim <52204621+Yeachan-Kim@users.noreply.github.com>
2024-01-09 17:17:55 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    url: "https://api.openai.com/v1/chat/completions",
+    embedding_url: "https://api.openai.com/v1/embeddings",
+    key: "sk-~",
+    chat_model : "gpt-3.5-turbo-1106"
+}
+
+module.exports= {
+    config
+}
